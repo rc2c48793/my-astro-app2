620c9f7b39bef57eaa3f6681b9693e1cb3423dec
rahul bisht <rahul@click2commission.com>
2026-02-13 11:41:11 +0530

    pack update

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,17 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 
+import cloudflare from '@astrojs/cloudflare';
+
 export default defineConfig({
   integrations: [react()],
   vite: { plugins: [tailwindcss()] },
+
   server: {
     host: '0.0.0.0',
     port: 5000,
     allowedHosts: true,
   },
+
+  adapter: cloudflare(),
 });
